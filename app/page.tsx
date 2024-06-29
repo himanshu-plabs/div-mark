2ec69044ea699e5767c8d9e1a73ddb511b6003fe
divyendra <betudst9107@gmail.com>
2024-06-30 03:43:45 +0530

    added prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Input } from "@/components/ui/input";
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  
   return (
     <main className="">
       <div>
