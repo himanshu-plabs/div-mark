99835804de572029e80c73ab4c91c9548c875b3d
divyendra <betudst9107@gmail.com>
2024-07-03 17:06:12 +0530

    added search on text

--- actions/SearchBookmark.ts
+++ actions/SearchBookmark.ts
@@ -3,27 +3,42 @@ import { db } from "@/lib/db";
 
 async function SearchBookmarks(tagsToSearch: string) {
   // Split the input string into an array of tags
-  
-    const tagsArray = tagsToSearch.split(",").map((tag) => tag.trim());
 
-    // Fetch all bookmarks
-    const bookmarks = await db.bookmark.findMany();
+  const tagsArray = tagsToSearch.split(",").map((tag) => tag.trim());
 
-    // Rank the bookmarks based on the number of matching tags
-    const rankedBookmarks = bookmarks
-      .map((bookmark) => {
-        const matchingTags = bookmark.tags.filter((tag) =>
-          tagsArray.includes(tag)
-        );
-        return {
-          ...bookmark,
-          matchCount: matchingTags.length,
-        };
-      })
-      .filter((bookmark) => bookmark.matchCount > 0)
-        .sort((a, b) => b.matchCount - a.matchCount);
-        return rankedBookmarks;
-  
+  // Fetch all bookmarks
+  const bookmarks = await db.bookmark.findMany({
+    where: {
+      OR: [
+        {
+          tags: {
+            has: tagsToSearch,
+          },
+        },
+        {
+          text: {
+            contains: tagsToSearch,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
+  return bookmarks;
+  // Rank the bookmarks based on the number of matching tags
+  // const rankedBookmarks = bookmarks
+  //   .map((bookmark) => {
+  //     const matchingTags = bookmark.tags.filter((tag) =>
+  //       tagsArray.includes(tag)
+  //     );
+  //     return {
+  //       ...bookmark,
+  //       matchCount: matchingTags.length,
+  //     };
+  //   })
+  //   .filter((bookmark) => bookmark.matchCount > 0)
+  //     .sort((a, b) => b.matchCount - a.matchCount);
+  //     return rankedBookmarks;
 }
 
 export default SearchBookmarks;
